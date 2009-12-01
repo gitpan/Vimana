a3d30356d979dc1ab7c8a24d31b9aac5cca01051
Yo-An Lin (林佑安) <cornelius@cpan.org>
2009-12-01 18:16:23 +0000

    Import of CORNELIUS/Vimana-2009.33602 from CPAN.
    gitpan-cpan-distribution: Vimana
    gitpan-cpan-version:      2009.33602
    gitpan-cpan-path:         CORNELIUS/Vimana-2009.33602.tar.gz
    gitpan-cpan-author:       CORNELIUS
    gitpan-cpan-maturity:     released

---
+++ lib/Vimana/Command/Installed.pm
@@ -0,0 +1,49 @@
+package Vimana::Command::Installed;
+use warnings;
+use strict;
+use base qw(App::CLI::Command);
+use YAML;
+use Vimana::Logger;
+use Vimana::PackageFile;
+use File::Find;
+
+=head2 run
+
+find installed packages.
+
+=cut
+
+sub run {
+    my $self = shift;
+    my @dir = ( File::Spec->join( $ENV{HOME}  , '.vim' , 'record' ) );
+    File::Find::find( sub { 
+        return unless -f $_;
+
+    # $File::Find::dir is the current directory name,
+    # $_ is the current filename within that directory
+    # $File::Find::name is the complete pathname to the file.
+    # 
+    # follow VIM::Packager META data
+    # 
+        my $record = YAML::LoadFile($_);
+        unless( $record ) {
+            print STDERR "ERROR: Record $_ load failed.\n";
+            return;
+        }
+        unless( $record->{meta} ) {
+            print STDERR "ERROR: Record $_ doesn't contain meta record.\n";
+            return;
+        }
+
+        unless( $record->{meta}{name} ) {
+            print STDERR "ERROR: Record $_ meta doesn't have package name.\n";
+            return;
+        }
+        print ' ' x 3 . $record->{meta}{name} . "\n";
+
+    } , @dir);
+}
+
+
+
+1;
---
+++ lib/Vimana/Record.pm
@@ -0,0 +1,70 @@
+package Vimana::Record;
+use warnings;
+use strict;
+use Vimana;
+use File::Path;
+use YAML;
+
+sub record_dir { return ( $ENV{VIMANA_BASE} || $ENV{HOME} ) . '/.vimana'; }
+
+sub record_file {  $_[0]->record_dir . '/index' }
+
+sub load {
+    my $class = shift;
+    my $path =  $class->record_dir;
+    if( ! -e $path ) {
+        File::Path::mkpath( $path );
+    }
+
+    my $record_file =  $class->record_file;
+    my $record = YAML::LoadFile( $record_file ) if -e $record_file;
+    return $record || {};
+}
+
+sub save {
+    my ($class,$new_record)= @_;
+    my $record_file = $class->record_file;
+    YAML::DumpFile( $record_file , $new_record );
+}
+
+
+sub find {
+    my ($class,$cname) = @_;
+    my $record = $class->load();
+    return $record->{$cname} if defined $record->{ $cname };
+}
+
+sub add {
+    my ( $class, $info ) = @_;
+    my $record = $class->load;
+
+    if( defined $record->{ $info->{cname} } ) {
+        return 0;
+    }
+
+    $record->{  $info->{cname}  } = $info;
+    $class->save( $record );
+    return $record;
+}
+
+=pod
+
+    # XXX: check if cname conflicts
+    sub set {
+        my $class = shift;
+        my %args = @_;
+        my $recordset = $class->get_recordset();
+        $recordset->{ $args{cname} } = {
+            cname => $args{cname} , 
+            files => [ ],
+            type  => undef,
+            install_date  => undef,
+            %args,
+        };
+        $class->set_recordset( $recordset );
+    }
+
+=cut
+
+
+1;
