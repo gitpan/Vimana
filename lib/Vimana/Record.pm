1e21bb6b78a3589637b625d547314a85e9151068
Yo-An Lin (林佑安) <cornelius@cpan.org>
2010-01-22 17:45:25 +0000

    Import of CORNELIUS/Vimana-2010.02301 from CPAN.
    gitpan-cpan-distribution: Vimana
    gitpan-cpan-version:      2010.02301
    gitpan-cpan-path:         CORNELIUS/Vimana-2010.02301.tar.gz
    gitpan-cpan-author:       CORNELIUS
    gitpan-cpan-maturity:     released

--- lib/Vimana/Record.pm
+++ lib/Vimana/Record.pm
@@ -3,68 +3,113 @@ use warnings;
 use strict;
 use Vimana;
 use File::Path;
+use Digest::MD5 qw(md5_hex);
 use YAML;
 
-sub record_dir { return ( $ENV{VIMANA_BASE} || $ENV{HOME} ) . '/.vimana'; }
-
-sub record_file {  $_[0]->record_dir . '/index' }
-
-sub load {
-    my $class = shift;
-    my $path =  $class->record_dir;
-    if( ! -e $path ) {
-        File::Path::mkpath( $path );
+sub record_path  {
+    my ( $class, $pkgname ) = @_;
+    my $record_dir =  $ENV{VIM_RECORD_DIR} || File::Spec->join($ENV{HOME},'.vim','record') ;
+    if( ! -d $record_dir ) {
+        File::Path::mkpath( $record_dir );
     }
-
-    my $record_file =  $class->record_file;
-    my $record = YAML::LoadFile( $record_file ) if -e $record_file;
-    return $record || {};
+    return File::Spec->join( $record_dir , $pkgname );
 }
 
-sub save {
-    my ($class,$new_record)= @_;
-    my $record_file = $class->record_file;
-    YAML::DumpFile( $record_file , $new_record );
-}
+=head2 load
 
+load package record , returns a hashref which contains:
 
-sub find {
-    my ($class,$cname) = @_;
-    my $record = $class->load();
-    return $record->{$cname} if defined $record->{ $cname };
-}
-
-sub add {
-    my ( $class, $info ) = @_;
-    my $record = $class->load;
-
-    if( defined $record->{ $info->{cname} } ) {
-        return 0;
-    }
-
-    $record->{  $info->{cname}  } = $info;
-    $class->save( $record );
-    return $record;
-}
-
-=pod
-
-    # XXX: check if cname conflicts
-    sub set {
-        my $class = shift;
-        my %args = @_;
-        my $recordset = $class->get_recordset();
-        $recordset->{ $args{cname} } = {
-            cname => $args{cname} , 
-            files => [ ],
-            type  => undef,
-            install_date  => undef,
-            %args,
-        };
-        $class->set_recordset( $recordset );
+spec:
+    {
+        version => 0.1,
+        generated_by => 'Vimana [Version]'
+        install_type => 'auto',  # auto , make , rake ... etc
+        meta => {
+            author: Cornelius
+            email: cornelius.howl@gmail.com
+            libpath: ./
+            name: gsession.vim
+            script_id: 2885
+            type: plugin
+            version: 0.21
+            version_from: plugin/gsession.vim
+            vim_version:
+        },
+        files => [
+            { file => "/Users/c9s/.vim/plugin/gsession.vim", checksum => "md5checksum" }
+        ]
     }
 
 =cut
 
+sub load {
+    my ( $class, $pkgname ) = @_;
+    my $record_file =  $class->record_path( $pkgname );
+
+    if( ! -e $record_file ) {
+        print STDERR "Package $pkgname is not installed.\n";
+        return ;
+    }
+
+    my $record = YAML::LoadFile( $record_file );
+    unless( $record ) {
+        print STDERR "Can not load record\n";
+        return ;
+    }
+    return $record;
+}
+
+sub remove {
+    my ( $class , $pkgname ) = @_;
+    my $record = $class->load( $pkgname );
+    return unless $record;
+
+    my $files = $record->{files};
+    print "Removing package $pkgname\n";
+    for my $entry ( @$files ) {
+        # XXX: check digest here
+        print "\tRemoving @{[ $entry->{file} ]}\n";
+        unlink $entry->{file};
+    }
+
+    print "Removing record\n";
+    my $file = $class->record_path( $pkgname );
+    unlink $file;
+    print "Done\n";
+}
+
+sub add {
+    my ( $class , $record ) = @_;
+    my $pkgname = $record->{package};
+    unless( $pkgname ) {
+        die "Package name is not declared.";
+    }
+
+    my $record_file =  $class->record_path( $pkgname );
+    return 0 if -f $record_file;
+    return YAML::DumpFile( $record_file , $record  );
+}
+
+sub mk_file_digest {
+    my ($self,$file) = @_;
+    open my $fh , "<" , $file;
+    binmode $fh,':bytes';
+    local $/;
+    my $content = <$fh>;
+    close $fh;
+    return md5_hex( $content );
+}
+
+sub mk_file_digests {
+    my $self = shift;
+    my @files = @_;
+    my @e = ();
+    for my $f ( @files ) {
+        my $ctx = $self->mk_file_digest( $f );
+        # print " $ctx: $f\n";
+        push @e, { file => $f , checksum => $ctx };
+    }
+    return @e;
+}
 
 1;
